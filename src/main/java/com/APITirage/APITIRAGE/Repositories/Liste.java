55a3e642bea883b5487b114109fb2195a1ebc274
mccamara <camaramamady9160@gmail.com>
2022-08-19 15:43:43 +0000

    Les repositories en ajoutant Jparepository

--- src/main/java/com/APITirage/APITIRAGE/Repositories/Liste.java
+++ src/main/java/com/APITirage/APITIRAGE/Repositories/Liste.java
@@ -1,6 +1,7 @@
 package com.APITirage.APITIRAGE.Repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface Liste extends CrudRepository<Liste,Long> {
+public interface Liste extends JpaRepository<Liste,Long> {
 }
--- src/main/java/com/APITirage/APITIRAGE/Repositories/Postulant.java
+++ src/main/java/com/APITirage/APITIRAGE/Repositories/Postulant.java
@@ -1,6 +1,7 @@
 package com.APITirage.APITIRAGE.Repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface Postulant extends CrudRepository<Postulant,Long> {
+public interface Postulant extends JpaRepository<Postulant,Long> {
 }
