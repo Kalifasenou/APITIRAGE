c25954857c835ef237fbc492d0db39bfc60da1ea
mccamara <camaramamady9160@gmail.com>
2022-08-20 12:44:15 +0000

    Methode import

--- src/main/java/com/APITirage/APITIRAGE/Services/PostulantService.java
+++ src/main/java/com/APITirage/APITIRAGE/Services/PostulantService.java
@@ -2,8 +2,12 @@ package com.APITirage.APITIRAGE.Services;
 
 import com.APITirage.APITIRAGE.Modeles.Postulant;
 
+import java.util.List;
+
 public interface PostulantService {
     // Fonctionnalité d'ajout d'un postulant ou des postulants
     void importer(Postulant postulants);
+
+    List<Postulant> lire();
 
 }
