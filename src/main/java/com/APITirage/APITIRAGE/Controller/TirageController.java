cb8740473d58cb638aa6c94ffb32911f9af1d15b
Aboubacar DJENIKA <75429330+geekdjenika@users.noreply.github.com>
2022-08-24 12:57:34 +0000

    --- Suppression des postulants de la liste une fois selectionnés ---

--- src/main/java/com/APITirage/APITIRAGE/Controller/TirageController.java
+++ src/main/java/com/APITirage/APITIRAGE/Controller/TirageController.java
@@ -7,6 +7,7 @@ import com.APITirage.APITIRAGE.Modeles.Tirage;
 import com.APITirage.APITIRAGE.Services.*;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,18 +29,21 @@ public class TirageController {
 
     public Object create(@RequestBody Tirage tirage, @PathVariable String libelleliste, @PathVariable long nbre){
         try {
+
             Liste liste =listeService.trouverListeParLibelle(libelleliste);
             List<Postulant> post= postulantService.TrouverIdPostList(liste.getId());
 
-            List<Postulant> lp= tirageService.creer(tirage,post,nbre);
+            Object lp= tirageService.creer(tirage,post,nbre);
             Long id_tirage =tirageService.trouverTirageParLibelle(tirage.getLibelletirage()).getId();
-            for (Postulant p :lp){
+            for (Postulant p :(List<Postulant>)lp){
 
                 postulantsTireService.creer(p.getId(),p.getNom(),p.getPrenom(),p.getNumero(),p.getEmail(),id_tirage);
                 postulantServiceIml.supprimerPostulant(p.getId());
             }
             return postulantsTireService.lirePostulantTireparnom(tirage.getLibelletirage());//"Tirage effectué avec succès ! \uD83D\uDE09 \uD83D\uDE09";
-        } catch (Exception e) {
+        } catch (IllegalArgumentException e) {
+            return Message.ErreurReponse("\uD83D\uDE1E☹️ Vous ne pouvez pas tirer "+nbre+" du nombre de postulants restants", HttpStatus.OK);
+        } catch (IncorrectResultSizeDataAccessException e) {
             return Message.ErreurReponse("Ce tirage est déjà effectué !\uD83D\uDE1E☹️ ", HttpStatus.OK);
         }
 
